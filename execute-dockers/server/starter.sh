b99b89e714c11d7b4f8492fdcabe2a8d3b5a223b
Ali <ali@Alis-MacBook-Pro.local>
2018-02-27 13:50:07 +0330

    Update server image

--- execute-dockers/server/starter.sh
+++ execute-dockers/server/starter.sh
@@ -2,7 +2,7 @@ export AICClient0Name=$client0_id
 export AICClient0Token=$client0_token
 export AICClient1Name=$client1_id
 export AICClient1Token=$client1_token
-TIMELIMIT=360
+TIMELIMIT=660
 
 echo 'unzip'
 unzip config.zip -d config/

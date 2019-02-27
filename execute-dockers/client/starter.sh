72df4830155aa160cb2680ee605a6aeda952a7e7
Iman Gholami <imangholami77@gmail.com>
2019-02-28 02:26:47 +0330

    Add clinet logs

--- execute-dockers/client/starter.sh
+++ execute-dockers/client/starter.sh
@@ -10,6 +10,6 @@ chown -R runner:runner client
 
 cd client
 #sudo -E -u runner -s /bin/bash run.sh
-/bin/bash run.sh
+/bin/bash run.sh | tail -c 10000000 | tee -a /game/client.log
 
 echo 'Client terminated.'
